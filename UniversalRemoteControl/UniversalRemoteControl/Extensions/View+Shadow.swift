179346a575ae4ea721758fe90b6f71d2cd6758ef
jb <jbdubillard@hotmail.com>
2024-10-05 17:43:40 +0200

    Fix indentation

--- UniversalRemoteControl/UniversalRemoteControl/Extensions/View+Shadow.swift
+++ UniversalRemoteControl/UniversalRemoteControl/Extensions/View+Shadow.swift
@@ -12,18 +12,18 @@ extension View {
         self
             .foregroundStyle(
                 .black.opacity(0.2)
-                    .shadow(
-                        .inner(color: .white.opacity(0.3), radius: 10, x: -5, y: -10)
-                    )
-                    .shadow(
-                        .inner(color: .black.opacity(0.3), radius: 10, x: 10, y: 10)
-                    )
-                    .shadow(
-                        .drop(color: .white.opacity(0.3), radius: 3, x: -3, y: -3)
-                    )
-                    .shadow(
-                        .drop(color: .black.opacity(0.3), radius: 4, x: 7, y: 7)
-                    )
+                .shadow(
+                    .inner(color: .white.opacity(0.3), radius: 10, x: -5, y: -10)
+                )
+                .shadow(
+                    .inner(color: .black.opacity(0.3), radius: 10, x: 10, y: 10)
+                )
+                .shadow(
+                    .drop(color: .white.opacity(0.3), radius: 3, x: -3, y: -3)
+                )
+                .shadow(
+                    .drop(color: .black.opacity(0.3), radius: 4, x: 7, y: 7)
+                )
             )
     }
 }
