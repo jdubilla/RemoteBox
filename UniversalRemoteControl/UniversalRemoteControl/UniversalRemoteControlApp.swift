4ace114bc6e911a6a329583f519f5067593f3b63
jb <jbdubillard@hotmail.com>
2024-10-05 17:41:46 +0200

    La mise en œuvre initiale de la télécommande est terminée pour le moment

---
+++ UniversalRemoteControl/UniversalRemoteControl/UniversalRemoteControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalRemoteControlApp.swift
+//  UniversalRemoteControl
+//
+//  Created by Jean-baptiste DUBILLARD on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct UniversalRemoteControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RemoteView()
+        }
+    }
+}
